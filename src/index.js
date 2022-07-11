4992560c52c816117e3304fc28520082ebd2466d
Francisco Lourenço <franciscolour2011@gmail.com>
2022-07-11 14:23:27 +0100

    delete todo

--- src/index.js
+++ src/index.js
@@ -57,19 +57,19 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
     const { user } = request;
     const { id } = request.params;
-    const { title, deadline } = request.body;
     const todo = user.todos.find(result => (result.id === id));
     if (!todo) return response.status(400).json({ error: 'Todo not found' });
     todo.done = true
     return response.status(201).json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-    // Complete aqui
-});
-
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-    // Complete aqui
+    const { user } = request;
+    const { id } = request.params;
+    const todo = user.todos.find(result => (result.id === id));
+    if (!todo) return response.status(400).json({ error: 'Todo not found' });
+    user.todos.splice(todo, 1);
+    return response.status(201).json(user.todos);
 });
 
 module.exports = app;
